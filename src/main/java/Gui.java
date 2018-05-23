52b05cd33453365f01c348ea41b082773d8f5211
Emre Gonen <emre.gonen@asyasoft.net>
2018-05-23 15:45:56 +0300

    Added empty gui with test button

---
+++ src/main/java/Gui.java
@@ -0,0 +1,10 @@
+import lombok.Data;
+
+import javax.swing.*;
+
+@Data
+public class Gui {
+
+    private JButton testButton;
+    private JPanel GuiPanel;
+}
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,10 +1,15 @@
+import javax.swing.*;
+
 public class Main {
 
 	public static void main(String[] args) {
 
-	    System.out.println("A new begining...");
+	    Gui gui = new Gui();
+	    JFrame frame = new JFrame("Sentiment Analysis & Artificial Data");
+	    frame.setContentPane(gui.getGuiPanel());
+	    frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+	    frame.pack();
+	    frame.setVisible(true);
 
 	}
-	 
-
 }
